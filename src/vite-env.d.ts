a1e35506c4d5b40919e2eeb94213c2bda155e1f2
cwanii66 <55499491+cwanii66@users.noreply.github.com>
2023-03-25 10:06:30 +0800

    chore: repo init

---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_USENAME: string
+  VITE_AGE: number
+}
+
---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import type { CommonServerOptions } from 'vite'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import dotenv, { DotenvParseOutput } from 'dotenv'
+
+// https://vitejs.dev/config/
+export default defineConfig((envConfig) => {
+  console.log({ envConfig })
+
+  // concat current filename
+  const envFileName: string = '.env'
+  const curEnvFileName = `${envFileName}.${envConfig.mode}`
+
+  // config for env of development mode
+  const curEnv: string = envConfig.mode
+  let server: CommonServerOptions = {}
+
+  const envData = fs.readFileSync(curEnvFileName)
+  const envMap: DotenvParseOutput = dotenv.parse(envData)
+  
+  if (curEnv === 'development') {
+    server = {
+      port: envMap.VITE_PORT,
+      host: envMap.VITE_HOST,
+      proxy: {
+        [envMap.VITE_BASE_URL]: {
+          target: envMap.VITE_PROXY_DOMAIN,
+        }
+      }
+    }
+
+    console.log('env -> development', server)
+  } else if (curEnv === 'production') {
+    console.log('env -> production', server)
+    server = {
+      port: envMap.VITE_PORT,
+      host: envMap.VITE_HOST,
+    }
+  }
+  return {
+    plugins: [
+      vue(),
+    ],
+    // server,
+  }
+})
+
