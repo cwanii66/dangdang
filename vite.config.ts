6f98e7bca20bf70b621343b57b70147afd102b51
cwanii66 <55499491+cwanii66@users.noreply.github.com>
2023-03-30 21:40:21 +0800

    chore: add inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import type { DotenvParseOutput } from 'dotenv'
 import dotenv from 'dotenv'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig((envConfig) => {
@@ -44,6 +45,10 @@ export default defineConfig((envConfig) => {
   return {
     plugins: [
       vue(),
+      Inspect({
+        build: true,
+        outputDir: '.vite-inspect',
+      }),
     ],
     // server,
   }
